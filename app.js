9adf35c209dee33c9042ac0f535dbb7413778ec4
nick-dolf <nickdolf@gmail.com>
2022-05-10 10:36:43 -0500

    hello world node setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const PORT = process.env.PORT || 5000
+const express = require('express')
+const app = express()
+
+let message = "Welcome to Aoclock"
+
+if (process.env.NODE_ENV === 'production') {
+  message += " - production environment"
+} else {
+  console.log("started node in development mode")
+  message += " - development environment"
+}
+
+app.get('/', (req, res) => {
+  res.send(message)
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`) )
